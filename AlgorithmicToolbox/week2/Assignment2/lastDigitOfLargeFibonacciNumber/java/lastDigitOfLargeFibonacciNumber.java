1a2d7e9ebef773dbbf4cfcfe78a819fdde8dd991
Nilesh Bhaskar <nileshbhaskar1113@gmail.com>
2022-09-12 22:56:06 +0530

    removed the package name

--- AlgorithmicToolbox/week2/Assignment2/lastDigitOfLargeFibonacciNumber/java/lastDigitOfLargeFibonacciNumber.java
+++ AlgorithmicToolbox/week2/Assignment2/lastDigitOfLargeFibonacciNumber/java/lastDigitOfLargeFibonacciNumber.java
@@ -1,11 +1,11 @@
-package lastDigitOfLargeFibonacciNumber;
+
 import java.util.*;
 public class lastDigitOfLargeFibonacciNumber {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n+1];
-        if(n<1)
+        if(n<=1)
         {
             System.out.println(n);
             return;
